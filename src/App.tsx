2d5afa2d981848f4ef635cdb8f29c6f9ded0cafb
artt1c <roma.myalo000@gmail.com>
2025-04-06 23:07:52 +0300

    memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Menu from "./Menu";
 
 function App() {
 
+  const [number, setNumber] = useState<number>(0)
+
+  useEffect(() => {
+    console.log('App build');
+  });
 
   return (
     <div className="App">
+      <Menu/>
+      <h2>{number}</h2>
 
+      <button onClick={()=>{
+        setNumber(prevState => prevState + 1);
+      }}>Increment</button>
     </div>
   );
 }
---
+++ src/Menu.tsx
@@ -0,0 +1,14 @@
+import React, {memo} from 'react';
+
+const Menu = memo(() => {
+
+  console.log('Menu build');
+
+  return (
+    <div>
+      Menu
+    </div>
+  );
+});
+
+export default Menu;
