5f6c75f591b4854fa3bfdd866c904240466f361a
artt1c <roma.myalo000@gmail.com>
2025-04-06 23:53:10 +0300

    useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import './App.css';
 import Menu from "./Menu";
 
 function App() {
 
   const [number, setNumber] = useState<number>(0)
+
+  const [state1, setState1] = useState<string>('users')
+  const [state2, setState2] = useState<string>('posts')
+  const [state3, setState3] = useState<string>('comments')
 
   useEffect(() => {
     console.log('App build');
@@ -14,9 +18,14 @@ function App() {
     console.log('asdff');
   },[])
 
+  const links = useMemo(() => {
+    return [state1, state2, state3]
+  }, [state1, state2, state3]);
+
   return (
     <div className="App">
-      <Menu id={0} someFn={someFn} />
+
+      <Menu id={0} someFn={someFn} links={links} />
       <h2>{number}</h2>
 
       <button onClick={()=>{
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -3,16 +3,19 @@ import React, {memo} from 'react';
 type MemoTypes = {
   id: number;
   someFn: () => void;
+  links: string[];
 }
 
-const Menu = memo(({id, someFn}:MemoTypes) => {
+const Menu = memo(({id, someFn, links}:MemoTypes) => {
 
   console.log('Menu build');
 
   return (
-    <div>
-      Menu
-    </div>
+    <ul>
+      {
+        links.map(link => (<li>{link}</li>))
+      }
+    </ul>
   );
 });
 
