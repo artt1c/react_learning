570c190a489821d0f91b74a96336a89a067b3547
artt1c <roma.myalo000@gmail.com>
2025-04-06 22:43:50 +0300

    Custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import {useFetch} from "./hooks/useFetch";
 
 function App() {
 
+  const users = useFetch();
 
   return (
     <div className="App">
-
+      {
+        users.map((user) => (
+          <div>{user.name}</div>
+        ))
+      }
     </div>
   );
 }
