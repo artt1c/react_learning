2d865dd561ec7a3e39e6e273e40e700d1f4a2797
artt1c <roma.myalo000@gmail.com>
2025-04-06 21:33:18 +0300

    useState useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
 
+  let [number, setNumber] = useState<number>(0)
+
+  useEffect(() => {
+    console.log('connect, send ' + number);
+    return () => {
+      console.log('unconnect')
+    }
+  }, [number]);
 
   return (
     <div className="App">
+      <h2>{number}</h2>
 
+      <button onClick={() => {
+        setNumber(prevState => prevState+1)
+      }}>Increment</button>
+
+      <button onClick={() => {
+        setNumber(prevState => prevState-1)
+      }}>Decrement</button>
     </div>
   );
 }
